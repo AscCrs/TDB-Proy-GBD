52b99f737ac7d71bdea4a291f7f65a3af2548bcc
AscCrs <cristoe2003@gmail.com>
2023-10-20 17:53:05 -0600

    Se modifico el archivo principal de la aplicacion

--- src/main/java/com/itver/gbd/GBD.java
+++ src/main/java/com/itver/gbd/GBD.java
@@ -3,45 +3,52 @@
  */
 
 package com.itver.gbd;
-import conn.MySQLConnection;
-import java.sql.SQLException;
-import java.util.Scanner;
+//import conn.MySQLConnection;
+//import conn.Conexion;
+//import java.sql.SQLException;
+//import java.util.Scanner;
+import vistas.*;
 
 /**
  *
  * @author crist
  */
 public class GBD {
+//    public static void main(String[] args) {
+//        String schemaN, ip, port;
+//        MySQLConnection connSQL = new MySQLConnection();
+//        Conexion conn = new Conexion();
+//        Scanner sc = new Scanner(System.in);
+//
+//        System.out.println("Ingrese una ip: ");
+//        ip = sc.nextLine();
+//        System.out.println("Ingresa un puerto: ");
+//        port = sc.nextLine();
+//
+//        conn.setURL("mysql", ip, port);
+//
+//        try {
+//            connSQL.setConnection(conn);
+//            connSQL.openConnection();
+//            System.out.print("Ingrese el nombre del schema: ");
+//            schemaN = sc.next();
+//            connSQL.setSchema(schemaN);
+//            connSQL.connectSchema();
+//
+//            connSQL.showSchemas();
+//        } catch (SQLException e) {
+//            e.printStackTrace();
+//        } finally {
+//            try {
+//                connSQL.closeConnection();
+//            } catch (SQLException e) {
+//                e.printStackTrace();
+//            }
+//        }
+//        sc.close();
+//    }
     public static void main(String[] args) {
-        String schemaN, ip, port;
-        MySQLConnection connSQL = new MySQLConnection();
-        Scanner sc = new Scanner(System.in);
-        
-        System.out.println("Ingrese una ip: ");
-        ip = sc.nextLine();
-        System.out.println("Ingresa un puerto: ");
-        port = sc.nextLine();
-        
-        connSQL.setIp(ip);
-        connSQL.setPort(port);
-        
-        try {
-            connSQL.openConnection();
-            System.out.print("Ingrese el nombre del schema: ");
-            schemaN = sc.next();
-            connSQL.setSchema(schemaN);
-            connSQL.connectSchema();
-            
-            connSQL.showSchemas();
-        } catch (SQLException e) {
-            e.printStackTrace();
-        } finally {
-            try {
-                connSQL.closeConnection();
-            } catch (SQLException e) {
-                e.printStackTrace();
-            }
-        }
-        sc.close();
+        Ventana_Ini conexion = new Ventana_Ini();
+        conexion.setVisible(true);
     }
 }
