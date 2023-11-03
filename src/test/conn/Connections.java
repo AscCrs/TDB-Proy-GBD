40f8d2ed3eb33d34cbd01a5a9a66267b9ed00cf2
AscCrs <cristoe2003@gmail.com>
2023-11-02 19:34:59 -0600

    Modificados archivos de Configuracion

---
+++ src/test/conn/Connections.java
@@ -0,0 +1,137 @@
+package conn;
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author crist
+ */
+
+// Bibliotecas para trabajar con el archivo Json
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
+
+import java.io.*;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+
+public class Connections {
+    private ArrayList<Conexion> conexiones = new ArrayList<>();
+    public static final String fileName = "connections.json";
+
+    public Connections() {
+        // Cargar las conexiones desde el archivo JSON al inicializar
+        crearJson();
+        cargarConexionesDesdeArchivo();
+    }
+    
+    public void crearJson() {
+        File file = new File(fileName);
+        
+        if (file.exists()) {
+            System.out.println("Archivo JSON recuperado exitosamente.");
+        } else {
+            // Crear el archivo JSON y escribir datos de ejemplo
+            try (FileWriter writer = new FileWriter(fileName)) {
+                // Aquí puedes escribir datos de ejemplo si lo deseas
+                writer.write("{}");
+                System.out.println("Archivo JSON creado.");
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    public void agregarConexion(Conexion conexion) {
+        conexiones.add(conexion);
+        guardarConexionesEnArchivo();
+    }
+
+    public void guardarConexionesEnArchivo() {
+        try (Writer writer = new FileWriter(fileName)) {
+            Gson gson = new Gson();
+            Map<String, Conexion> map = new HashMap<>();
+            int i = 1;
+            for (Conexion conexion : conexiones) {
+                map.put("Conexion" + i, conexion);
+                i++;
+            }
+            gson.toJson(map, writer);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void cargarConexionesDesdeArchivo() {
+        try (Reader reader = new FileReader(fileName)) {
+            Gson gson = new Gson();
+            TypeToken<Map<String, Conexion>> typeToken = new TypeToken<Map<String, Conexion>>() {
+            };
+            Map<String, Conexion> map = gson.fromJson(reader, typeToken.getType());
+            if (map != null) {
+                for (Conexion conexion : map.values()) {
+                    conexiones.add(conexion);
+                }
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public ArrayList<Conexion> getConexiones() {
+        return conexiones;
+    }
+    
+    public ArrayList<String> getNombresConexiones() {
+        ArrayList<String> nombres = new ArrayList<>();
+        for (Conexion conexion : conexiones) {
+            nombres.add(conexion.nombreCon);
+        }
+        return nombres;
+    }
+
+    public Conexion buscarConexion(String nombre) {
+        for (Conexion conexion : this.conexiones) {
+            if (conexion.nombreCon.equals(nombre)) {
+                return conexion;
+            }
+        }
+        return null;
+    }
+    
+    public void eliminarConexion(String nombre) {
+        Conexion conexionAEliminar = null;
+        for (Conexion conexion : conexiones) {
+            if (conexion.nombreCon.equals(nombre)) {
+                conexionAEliminar = conexion;
+                break;
+            }
+        }
+        if (conexionAEliminar != null) {
+            conexiones.remove(conexionAEliminar);
+            guardarConexionesEnArchivo();
+        }
+    }
+    
+    public void editarConexion(String nombre, String nuevaURL) {
+        for (Conexion conexion : conexiones) {
+            if (conexion.nombreCon.equals(nombre)) {
+                // Analizar la nueva URL para obtener la IP y el puerto
+                String[] partesURL = nuevaURL.split("//|:");
+                if (partesURL.length >= 4) {
+                    conexion.tipo = partesURL[1];
+                    conexion.ip = partesURL[2];
+                    conexion.port = partesURL[3];
+                    conexion.url = nuevaURL;
+                    guardarConexionesEnArchivo();
+                    break;
+                }
+            }
+        }
+    }
+
+}
---
+++ src/test/conn/MySQLConnection.java
@@ -0,0 +1,179 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package conn;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.sql.ResultSet;
+import conn.Conexion;
+import java.sql.DatabaseMetaData;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ *
+ * @author crist
+ */
+public class MySQLConnection {
+    // Configura tus credenciales y detalles de conexión
+    private Conexion conn;
+    private static String strStatement = "";
+    private String schemaName;
+    private static Connection connection; 
+    private static Statement statement;
+
+    public void setSchema(String nombre) {
+        this.schemaName = nombre;
+    }
+
+    public void setConnection(Conexion conn) {
+        this.conn = conn;
+    }
+
+    public String getStatement() {
+        return this.strStatement;
+    }
+    
+    // Metodo que crea el esquema 
+    public void connectSchema() {
+        try {
+            String createSchemaSQL = "DROP " + this.schemaName;
+            statement.executeUpdate(createSchemaSQL);
+            createSchemaSQL += "CREATE SCHEMA IF NOT EXISTS " + this.schemaName;
+            statement.executeUpdate(createSchemaSQL);
+            
+            System.out.println("Se creo correctamente el schema: " + this.schemaName);
+        } catch (SQLException e) {
+            System.err.println("Error al crear el schema: " + e.getMessage());
+        }
+    }
+    
+    // Metodo para crear una tabla
+    public void createTable(String nombreTabla) {
+        try {  
+            this.strStatement = "CREATE TABLE IF NOT EXIST" + nombreTabla;
+            statement.executeUpdate(this.strStatement);
+            System.out.println("Se creo correctamente la tabla");
+        } catch (SQLException e) {
+            System.err.print("Error al crear la tabla: " + e.getMessage());
+        }
+    }
+    
+    // Metodo para mostrar los esquemas disponibles
+    public List<String> showSchemas() {
+        List<String> schemaNames = new ArrayList<>();
+        try {
+            String createSchemaSQL = "SHOW SCHEMAS";
+            ResultSet resultSet = statement.executeQuery(createSchemaSQL);
+
+            System.out.println("Los Schema encontrados, son los siguientes: ");
+            while (resultSet.next()) {
+                String schemaName = resultSet.getString(1);
+                schemaNames.add(schemaName);
+                System.out.println("Schema: " + schemaName);
+            }
+        } catch (SQLException e) {
+            System.err.print("Error al listar los schema's: " + e.getMessage());
+        }
+        return schemaNames;
+    }
+    
+    public void createIndex(String tableName, String indexName, String columns) {
+        try {
+            String createIndexSQL = "CREATE INDEX " + indexName + " ON " + tableName + " (" + columns + ")";
+            statement.executeUpdate(createIndexSQL);
+            System.out.println("Se creó correctamente el índice: " + indexName + " en la tabla: " + tableName);
+        } catch (SQLException e) {
+            System.err.print("Error al crear el índice: " + e.getMessage());
+        }
+    }
+    
+    public void createTable(String nombreTabla, List<String> atributos, List<String> llavesPrimarias, List<String> llavesForaneas) {
+        try {
+            StringBuilder createTableSQL = new StringBuilder("CREATE TABLE IF NOT EXISTS " + nombreTabla + " (");
+
+            // Agregar atributos
+            for (String atributo : atributos) {
+                createTableSQL.append(atributo).append(", ");
+            }
+
+            // Agregar llaves primarias
+            if (!llavesPrimarias.isEmpty()) {
+                createTableSQL.append("PRIMARY KEY (");
+                for (String primaryKey : llavesPrimarias) {
+                    createTableSQL.append(primaryKey).append(", ");
+                }
+                createTableSQL.setLength(createTableSQL.length() - 2);
+                createTableSQL.append("), ");
+            }
+
+            // Agregar llaves foraneas
+            for (String foreignKey : llavesForaneas) {
+                createTableSQL.append(foreignKey).append(", ");
+            }
+
+            // Eliminar la coma final
+            createTableSQL.setLength(createTableSQL.length() - 2);
+
+            createTableSQL.append(")");
+
+            statement.executeUpdate(createTableSQL.toString());
+            System.out.println("Se creó correctamente la tabla: " + nombreTabla);
+        } catch (SQLException e) {
+            System.err.print("Error al crear la tabla: " + e.getMessage());
+        }
+    }
+    
+    public List<String> listTablesInDatabase(String databaseName) {
+        List<String> tableNames = new ArrayList<>();
+
+        try {
+            DatabaseMetaData metaData = connection.getMetaData();
+            ResultSet resultSet = metaData.getTables(databaseName, null, null, new String[]{"TABLE"});
+            
+            while (resultSet.next()) {
+                String tableName = resultSet.getString("TABLE_NAME");
+                tableNames.add(tableName);
+            }
+        } catch (SQLException e) {
+            System.err.println("Error al listar las tablas: " + e.getMessage());
+        }
+
+        return tableNames;
+    }
+    
+    // Método para establecer la conexión a la base de datos
+    public void openConnection() throws SQLException {
+        try {
+            Connections conexiones = new Connections();
+            if (connection == null || connection.isClosed()) {
+                Class.forName("com.mysql.cj.jdbc.Driver"); // Carga el controlador JDBC
+                connection = DriverManager.getConnection(conn.url, conn.user, conn.password);
+                statement = connection.createStatement();
+                System.out.println("Conexión exitosa a la base de datos MySQL.");
+                conexiones.agregarConexion(conn);
+            }
+        } catch (ClassNotFoundException e) {
+            System.err.println("Error al cargar el controlador JDBC: " + e.getMessage());
+        } catch (SQLException e) {
+            System.err.println("Error al conectar a la base de datos: " + e.getMessage());
+        }
+    }
+
+    // Método para cerrar la conexión
+    public void closeConnection() throws SQLException {
+        try {
+            if (statement != null && !statement.isClosed()) connection.close();
+            if (connection != null && !connection.isClosed()) {
+                connection.close();
+                System.out.println("Conexión cerrada.");
+            } 
+        } catch (SQLException e) {
+            System.err.println("Error al cerrar la conexión: " + e.getMessage());
+        }
+    }
+}
